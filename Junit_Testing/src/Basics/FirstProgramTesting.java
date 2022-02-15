edf42155e23f1607e51a38796ac39ed75a7f5456
Minal <Minal@DESKTOP-298FHC6>
2022-02-15 18:39:19 +0530

    First Commit

---
+++ Junit_Testing/src/Basics/FirstProgramTesting.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstProgramTesting {
+	
+	@Test
+	void dispaly() {
+		System.out.println("Hello World");
+		
+	}
+	
+	@Test
+	void dispaly1() {
+		System.out.println("Hii Students");
+		
+	}
+
+}
